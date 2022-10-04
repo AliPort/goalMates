3150b6e664d01a8c63f51cba0644dc0974dcf6d8
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-10-03 21:37:05 -0500

    adding apiKeys.js
    Please do merge this file

---
+++ src/components/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{7e087e5a6f86538d14bedac2066c2627}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
