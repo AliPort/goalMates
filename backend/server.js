6ff5fbe2171624e5c6c8d092424ee2e86292aa25
cre8ivesolutions <tammerz78@gmail.com>
2022-10-03 21:39:43 -0500

    server workinggit add .!

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 const express = require('express')
-const PORT = process.env.PORT
-const app = express
+const app = express()
 const { Sequelize } = require('sequelize')
 const user = require('./controllers/user_controller')
 
+const PORT = process.env.PORT
 
 require('dotenv').config()
-app.use(express.urlencoded({ extended: false }))
-
-
-app.get('/', (req, res)=> {
-    res.send('welcome')
-})
-
-app.listen(PORT, ()=> console.log(`listening on port ${PORT}`))
+app.use(express.urlencoded(
+    { extended: false }
+    ))
+    
+    
+    app.get('/', (req, res)=> {
+        res.send('welcome')
+    })
+    
+    app.listen(PORT, ()=> console.log(`listening on port goalMates ${PORT}`))
+    
