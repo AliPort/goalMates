d37522a6579e7f9bb15d00ef993c02d2db461644
AliPort <101525919+AliPort@users.noreply.github.com>
2022-10-03 23:56:11 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import SignUp from './components/SignUp';
+import Goal from './components/Goal';
 
 function App() {
   return (
