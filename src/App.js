0a53ff3a0b43a0a32955c71582e657b5945d0f7d
AliPort <bwithtacos@gmail.com>
2022-10-03 20:41:46 -0500

    added goal to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import Goal from './components/Goal';
 
 function App() {
   return (
