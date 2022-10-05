28b076a5e6ab0efaca1e69ed6cd48b613fbe139a
cre8ivesolutions <tammerz78@gmail.com>
2022-10-04 20:10:54 -0500

    added react router to app.js, moved package and package-lpock inside frontend, added .gitignore to backend, added components to node modules

---
+++ frontend/src/components/GoalsView.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import Home from './Home';
+
+function GoalsView(){
+    return (
+        <h1>This is the Goals View component </h1>,
+        <h2>You can also create your own goals to find common goals</h2>
+    )
+}
+
+export default GoalsView
+
+
+// let [search, setSearch] = useState('')
+
+// const handleSearch = (e, term) => {
+//     e.preventDefault()
+//     setSearch(term)
+// }
+// console.log({data})
