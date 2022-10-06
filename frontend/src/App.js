b7bbebdcccbddd22f73cbfd0ace0d2b1bfd22842
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-10-06 14:44:09 -0500

    cleaning up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,13 +19,14 @@ import YourMates from './components/Pages/YourMates';
 
 // import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div className="App">
     
-
+      
       <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
