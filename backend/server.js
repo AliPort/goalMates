f7f2d8a66cf32dcd878af18ad2732b1cf4b2649e
cre8ivesolutions <tammerz78@gmail.com>
2022-10-08 12:18:35 -0500

    added code to make the searchbar work, everything should work except we need to get the backend server into the frontend/src folder

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-const express = require('express')
-const app = express()
-const { Sequelize } = require('sequelize')
-const user = require('./controllers/user_controller')
+const express = require("express");
+const app = express();
+const { Sequelize } = require("sequelize");
+import user from "./src/controllers/user_controller.js";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-require('dotenv').config()
-app.use(express.urlencoded(
-    { extended: false }
-    ))
-    
-    
-    app.get('/', (req, res)=> {
-        res.send('welcome')
-    })
-    
-    app.listen(PORT, ()=> console.log(`listening on port goalMates ${PORT}`))
-    
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+app.listen(PORT, () => console.log(`listening on port goalMates ${PORT}`));
+
+module.export = goalMates_server;
