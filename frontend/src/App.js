c4389a8a142712aef190d27c319d24a642e834b9
Crystal Korth <crystalkorth@gmail.com>
2022-10-09 13:27:50 -0500

    moved switch from top:0 to top: 100

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,45 +12,18 @@ import Weather from './Weather';
 import Home from './components/Pages/Home';
 import SignUpPage from './components/Pages/SignUpPage';
 import About from './components/Pages/About';
-<<<<<<< HEAD
-// import Contact from './components/Pages/Contact';
-// import GoalsPage from './components/Pages/GoalsPage';
-import YourMates from './components/Pages/YourMates';
-// import Goal from './components/Pages/Goal';
-
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar';
-import { HamburgerMenu } from './components/hamburgerMenu';
-import Register from './components/Pages/Register';
-=======
 
 // import { useState } from 'react';
 import { BrowserRouter as Router,  Route , Routes} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import HamburgerMenu from './components/hamburgerMenu/HamburgerMenu';
 
->>>>>>> d8b57a0359e48ae6bebc5b9f26e45618e87ffbd2
 
 function App() {
 
 
   return (
     <div className="App">
-<<<<<<< HEAD
-    <Router>
-    <Navbar/>
-    <HamburgerMenu/>
-    <Routes>
-      
-          <Route path="/" element = {<Home/>} />  
-          <Route path="/about/" element= {<About/>} /> 
-          <Route path="/LogIn" element={<LogIn />} /> 
-          {/* <Route path="/WeatherMain" element={<WeatherMain/>} />  */}
-          <Route path='/signup' element={<SignUpPage/>}/>  
-          {<Route path='/register' element={<Register/>}/>}
-=======
->>>>>>> d8b57a0359e48ae6bebc5b9f26e45618e87ffbd2
 
       <Router>
       <Navbar />
@@ -58,28 +31,6 @@ function App() {
         
           <Routes>
 
-<<<<<<< HEAD
-   
-     
-     
-        
-        {/* <Route path="/newuser" element={<SignUpPage/>}/> */}
-        
-        {/* <Route path="/Mates" element={<YourMates/>} /> */}
-        {/* <Route path="/Goal" element={<Goal />} /> */}
-        {/* <Route path="/Goals" element={<GoalsPage />} />  */}
-        
-        {/* <Route path='/profile' element={<Profile/>} />  */}
-        {/* <Route path='/about' element={<About/>}/> */}
-        {/* <Route path='/contact' element={<Contact/>}/> */}
-       
-        
-      </Routes>
-      
-    </Router>
-    <Footer />
-    </div> 
-=======
           <Route path="/" element={<Home />} />
           <Route path="/about/" element={<About />} />
           <Route path="/Weather" element={<Weather />} />
@@ -111,7 +62,6 @@ function App() {
       </Router>
     
     </div>
->>>>>>> d8b57a0359e48ae6bebc5b9f26e45618e87ffbd2
   );
 }
 
